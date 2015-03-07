919a258bcb15bac93b98f40b1d91c79da7c061f4
横井一樹 <yokoikazuki@yokoikazuki.local>
2015-03-07 10:58:40 +0900

    Initial Commit

---
+++ Warranty Memo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Warranty Memo
+//
+//  Created by 横井一樹 on 2015/03/07.
+//  Copyright (c) 2015年 LifeisTech. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
